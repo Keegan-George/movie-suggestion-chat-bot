25bf67ca4f511df7f17d84926c88f8714274bfa0
Keegan-George <39040251+Keegan-George@users.noreply.github.com>
2025-08-21 02:06:20 -0400

    Replace QTextEdit with QListWidget

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 from PyQt6.QtWidgets import (
     QGridLayout,
     QWidget,
-    QTextEdit,
     QLineEdit,
     QPushButton,
+    QListWidget,
+    QListWidgetItem,
 )
+
+from PyQt6.QtCore import Qt
 
 
 class Window(QWidget):
@@ -14,9 +17,7 @@ class Window(QWidget):
         self.setWindowTitle("Movie Suggestion Chat Bot")
 
         # widgets
-        self.chat = QTextEdit(self)
-        self.chat.setReadOnly(True)
-        self.chat.setAcceptRichText(True)
+        self.chat = QListWidget(self)
         self.input = QLineEdit(self)
         self.input.setFocus()
         self.submit_button = QPushButton("Submit", self)
@@ -33,23 +34,12 @@ class Window(QWidget):
         Display text in the chat window.
         """
 
-        chat_bubble = f"""
-            <table width="100%" cellspacing="0" cellpadding="4">
-                <tr>
-                    {'<td width="60%">' if sender == 'user' else '<td width="40%"></td><td width="60%">'}
-                        <div style="
-                            background-color:{'#FFD580' if sender == 'user' else '#C1E1C1'};
-                            border-radius: 12px;
-                            font-size: 12px;
-                            padding: 4px;
-                            color: black;
-                            font-family: Courier;
-                        ">
-                            {text}
-                        </div>
-                    </td>
-                </tr>
-            </table>
-        """
+        chat_bubble = QListWidgetItem(text)
 
-        self.chat.insertHtml(chat_bubble)
+        if sender == "user":
+            chat_bubble.setTextAlignment(Qt.AlignmentFlag.AlignLeft)
+
+        elif sender == "chatbot":
+            chat_bubble.setTextAlignment(Qt.AlignmentFlag.AlignRight)
+
+        self.chat.addItem(chat_bubble)
